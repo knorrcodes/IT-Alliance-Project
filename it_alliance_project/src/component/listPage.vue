45aa2fb7a04ca265dc774df8aac81d02eb95f0d5
Darrin Knorr <knorr.darrin@gmail.com>
2020-03-31 13:01:23 -0500

    Updated data on the list page

--- it_alliance_project/src/component/listPage.vue
+++ it_alliance_project/src/component/listPage.vue
@@ -4,13 +4,12 @@
     <br />
     <h1>Spring 2020</h1>
     <ul>
-      <li>IT Alliance Project</li>
-      <li>AI ChatBot for Customer Serice with SMS Text messenger Bot</li>
-      <li>Champion Lab</li>
-      <li>Evansville Association for the Blind</li>
-      <li>Silent Auction for ECTA (Evansville Community Tennis Association)</li>
-      <li>Vanderburgh County Fair</li>
-      <li>Web Service For Sales Tasks</li>
+      <li onclick="alert('Team name: IT Alliance \nMembers: Darrin Knorr, Joe Massaro, Bassim Alamer, Josh Martin \nSemester: Spring 2020')">IT Alliance Project</li>
+      <li onclick="alert('Team name: AI ChatBot for Customer Serice with SMS Text messenger Bot \nMembers Albert Huang, Austin Steele, Edward Yevincy Quentin Peters, Elizabeth Fleming \n Semester: Spring 2020')" >AI ChatBot for Customer Serice with SMS Text messenger Bot</li>
+      <li onclick="alert('Team name: Champion Labs \nMembers: Payton Sharpe, Joshua Joines, Hans Lagenour, Austin Garrison, Nicholas Scheller \nSemester: Spring 2020')">Champion Lab</li>
+      <li onclick="alert('Team name: WAZE: city transportation analysis \nMembers: Sara Kniepmann, Austin Scherer, Celina Snyder, Kenlee Rumer, Alex Markle \nSemester: Spring 2020')">Evansville Association for the Blind</li>
+      <li onclick="alert('Team name: Evansville Association for the Blind \nMembers: Raquel Harvey, Nathan Hall, Garrett Hopf\nSemester: Spring 2020')">Silent Auction for ECTA (Evansville Community Tennis Association)</li>
+      <li onclick="alert('Team name: Silent Auction for ECTA (Evansville Community Tennis Association) \nMembers: Lee Keitel, Jace Anderson, Jeremy Hertel, Brandon Beach, Sky Kollak \nSemester: Spring 2020')">Vanderburgh County Fair</li>
     </ul>
   </div>
 </template>
@@ -20,7 +19,13 @@
     and return that informationa and display it in a pop up for the user to review. */
 
 export default /*class listPage extends Vue*/ {
-  name: "listPage"
+  name: "listPage",
+  methods:{
+    italliance: function(){
+      alert("test hello");
+    }
+  }
+  
 };
 </script>
 
