73e0c3dfcbf39af9853c2c00ebb81bf81ebb5200
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2020-04-16 12:50:45 -0500

    fixed router page errors

--- it_alliance_project/src/router.js
+++ it_alliance_project/src/router.js
@@ -6,64 +6,59 @@ import mainPage from '@/component/mainPage'
 import navbar from '@/component/navbar'
 import Auth from '@okta/okta-vue'
 
-Vue.use(Auth, {
-            issuer: 'https://dev-253896.okta.com/oauth2/default/',
+const config = {
+    clientId: '0oa242wfbnQmekjOF4x6',
+    issuer: 'https://dev-253896.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:8080/implicit/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true
+}
+Vue.use(Auth, config)
 
+const CALLBACK_PATH = '/implicit/callback';
 
-            const config = {
-                clientId: '0oa242wfbnQmekjOF4x6',
-                issuer: 'https://dev-253896.okta.com/oauth2/default',
-                redirectUri: 'http://localhost:8080/implicit/callback',
-                scopes: ['openid', 'profile', 'email'],
-                pkce: true
+const router = new Router({
+    mode: 'history',
+    routes: [{
+        path: CALLBACK_PATH,
+        component: Auth.handleCallback()
+    }, ]
+})
+Vue.use(Router);
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/listPage',
+            name: 'listPage',
+            component: listPage,
+            meta: {
+                requiresAuth: true
             }
-            Vue.use(Auth, config)
 
-            const CALLBACK_PATH = '/implicit/callback';
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: login
+        },
+        {
+            path: '/mainPage',
+            name: 'mainPage',
+            component: mainPage
+        },
+        {
+            path: '/',
+            name: 'default',
+            component: mainPage
+        },
+        {
+            path: '/navbar',
+            name: 'navbar',
+            component: navbar
+        },
 
-            const router = new Router({
-                mode: 'history',
-                routes: [{
-                    path: CALLBACK_PATH,
-                    component: Auth.handleCallback()
-                }, ]
-            })
-            Vue.use(Router);
-            router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
-
-
-            export default new Router({
-                mode: "history",
-                base: process.env.BASE_URL,
-                routes: [{
-                        path: '/listPage',
-                        name: 'listPage',
-                        component: listPage,
-                        meta: {
-                            requiresAuth: true
-                        }
-
-                    },
-                    {
-                        path: '/login',
-                        name: 'login',
-                        component: login
-                    },
-                    {
-                        path: '/mainPage',
-                        name: 'mainPage',
-                        component: mainPage
-                    },
-                    {
-                        path: '/',
-                        name: 'default',
-                        component: mainPage
-                    },
-                    {
-                        path: '/navbar',
-                        name: 'navbar',
-                        component: navbar
-                    },
-
-                ],
-            });
+    ],
+});
